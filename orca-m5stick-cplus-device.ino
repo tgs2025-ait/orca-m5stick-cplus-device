0b85aca8864e9c735491d8ca7588b07a5ba203fc
MMD-Lucky <factorytakutorii4989@gmail.com>
2025-06-16 17:21:11 +0900

    Bluetoothでのシリアル通信と加速度取得

--- orca-m5stick-cplus-device.ino
+++ orca-m5stick-cplus-device.ino
@@ -1,14 +1,38 @@
 #include <M5StickCPlus.h>
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+float ax, ay, az;
 
 void setup() {
-  M5.begin();
-  M5.Lcd.setRotation(3);
-  M5.Lcd.print("Hello M5!");
-  Serial.begin(115200);
-  Serial.println("setup end!");
+
+  M5.begin();//デバイス初期化
+  M5.IMU.Init();//IMU初期化
+
+  M5.Lcd.setRotation(3);//画面回転
+  M5.Lcd.fillScreen(BLACK);// 背景を黒でクリア
+  M5.Lcd.setTextFont(2);   // フォントサイズ設定
+  M5.Lcd.setTextSize(3);//テキストサイズ設定
+
+  SerialBT.begin(115200);//Bluetooth経由でのシリアル通信のボーレードを指定
+  SerialBT.begin("orca-m5stick-c-plus-device");//Bluetoothデバイス名
+
 }
 
 void loop() {
-  Serial.println("test");
-  delay(1000);
+
+  // 加速度データ取得
+  M5.IMU.getAccelData(&ax, &ay, &az);
+  // 表示位置・内容
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.printf("AX: %6.2f g\n", ax);
+  M5.Lcd.printf("AY: %6.2f g\n", ay);
+  M5.Lcd.printf("AZ: %6.2f g\n", az);
+
+  SerialBT.printf("AX: %6.2f g\n", ax);
+  SerialBT.printf("AY: %6.2f g\n", ay);
+  SerialBT.printf("AZ: %6.2f g\n", az);
+
+  delay(200);  // 更新間隔（200ms）
+  
 }
